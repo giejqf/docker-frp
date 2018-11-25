a28d259dc9ac550c10a1788a8dd00cc85e6e01ae
Qianfan <q.jiang16@imperial.ac.uk>
2018-11-25 19:55:41 +0000

    Update frp version, fix entrypoint

---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+CLIENTCFG="/frp/frpc.ini"
+SERVERCFG="/frp/frps.ini"
+
+if [ -f $CLIENTCFG ]; then
+    echo "Client config file found, starting frpc"
+    /frp/frp_0.21.0_linux_amd64/frpc -c $CLIENTCFG
+else
+    echo "Client config file not found"
+fi
+
+if [ -f $SERVERCFG ]; then
+    echo "Server config file found, starting frpc"
+    /frp/frp_0.21.0_linux_amd64/frps -c $SERVERCFG
+else
+    echo "Server config file not found"
+fi
